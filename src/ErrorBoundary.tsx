6c782c3e6b9ba28595255268c963427cf48c7380
the_zen <alayandezainab64@gmail.com>
2023-12-24 18:12:35 +0100

    request tab formatted

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,16 +1,58 @@
+// import React, { ReactNode, ErrorInfo } from 'react';
+
+// interface ErrorBoundaryProps {
+//   children: ReactNode;
+// }
+
+// class ErrorBoundary extends React.Component<ErrorBoundaryProps> {
+//   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+//     // Log the error or handle it as needed
+//     console.error(error, errorInfo);
+//   }
+
+//   render() {
+//     return this.props.children;
+//   }
+// }
+
+// export default ErrorBoundary;
+
+
 import React, { ReactNode, ErrorInfo } from 'react';
 
 interface ErrorBoundaryProps {
   children: ReactNode;
 }
 
-class ErrorBoundary extends React.Component<ErrorBoundaryProps> {
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     // Log the error or handle it as needed
     console.error(error, errorInfo);
+    this.setState({ hasError: true });
   }
 
   render() {
+    if (this.state.hasError) {
+      // Render fallback UI when an error occurs
+      return (
+        <div>
+          <h1>Something went wrong.</h1>
+          <p>Please refresh the page or try again later.</p>
+        </div>
+      );
+    }
+
     return this.props.children;
   }
 }
--- src/component/reusable/Sidebar.tsx
+++ src/component/reusable/Sidebar.tsx
@@ -1,26 +1,26 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 // import { useHistory } from "react-router-dom";
 import ORMLogo from "../../assets/svg/orm-logo.svg";
 import LMCLogo from "../../assets/svg/LMCLogo.svg";
 import { DASHBOARD_SIDE_BAR_LINKS } from "../../utils/constant/DashboardLinks";
 import RequestAccordion from "./RequestAccordion";
+import Profile from "../admin/Profile";
 
 
 const linkClasses =
   "flex items-center gap-3 text-gray-500 font-light px-9 py-2 cursor-pointer hover:bg-blue-100 hover:no-underline active:bg-neutral-600 rounded-sm text-base";
 
 const SideBar = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const [isRequestAccordionOpen, setIsRequestAccordionOpen] = useState(false);
-  // const [sideBarOpen, setSideBarOpen] = useState(false);
 
-  const handleNavigation = () => {
-
-    // if (key  === "dashboard") navigate("/dashboard");
-    // else if (key === "profile") navigate("/profile");
-    // else navigate("/settings")
+  const handleNavigation = (key: string) => {
+    console.log("key => ", key)
+    if (key  === "dashboard") navigate("/dashboard");
+    else if (key === "profile") navigate("/profile");
+    else navigate("/settings")
   };
   
 
@@ -28,9 +28,7 @@ const SideBar = () => {
     setIsRequestAccordionOpen(!isRequestAccordionOpen);
   };
 
-  // const toggleSideBar = () => {
-  //   setSideBarOpen(!sideBarOpen);
-  // };
+
 
   return (
     <div className="w-44 pt-0 mt-0 h-full items-center flex flex-col border overflow-hidden">
@@ -43,8 +41,8 @@ const SideBar = () => {
        {DASHBOARD_SIDE_BAR_LINKS.map((item) => (
         <div key={item.key}>
           <SideBarLink item={item} onClick={item.key === "request" ? toggleRequestAccordion : 
-                                            item.key === "dashboard" ? handleNavigation:
-                                            item.key === "profile" ? handleNavigation :
+                                            item.key === "dashboard" ? handleNavigation("dashboard"):
+                                            item.key === "profile" ? handleNavigation("profile") :
                                             item.key === "settings" ? handleNavigation : undefined} />
            {item.key === "request" && isRequestAccordionOpen && <RequestAccordion />}
            {item.key === "dashboard" && <Link key={item.key} to="/dashboard"></Link>}
